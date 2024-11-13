d5e23e8cf5bbfd6e8e9cc3b5ba11eb64481c75e7
Geir Marius Gjul (Innleid) <Geir.Marius.Gjul@fhi.no>
2024-11-13 16:42:32 +0100

    Fix failing tests on a "norwegian developer" machine.

--- Fhi.NINCheck.Test/ValidationTests.cs
+++ Fhi.NINCheck.Test/ValidationTests.cs
@@ -5,10 +5,10 @@ internal class ValidationTests
     [TestCase("55076500565", "Ukjent")]
     [TestCase("12345678901", "Ukjent")]
     [TestCase("01112835470", "FNummer")]
-    [TestCase("200112345609","DufNummer")]
-    [TestCase("81212121223","FHNummer")]
-    [TestCase("17454026641","HNummer")]
-    [TestCase("28894698995","TenorTestNummer,FNummer")] 
+    [TestCase("200112345609", "DufNummer")]
+    [TestCase("81212121223", "FHNummer")]
+    [TestCase("17454026641", "HNummer")]
+    [TestCase("28894698995", "TenorTestNummer,FNummer")]
     [TestCase("68126952442", "DNummer")]
     [TestCase("31739556891", "SyntPopTestNummer,FNummer")]
     public void CheckTypeOfNumber(string nin, string expected)
@@ -17,9 +17,8 @@ internal class ValidationTests
         Assert.That(nin.CheckNinType(), Is.EqualTo(expected));
     }
 
-
-    [TestCase("01814099829",1940)]  // Intervallet 900-999 (Personer født mellom 1940 og 1999), Issue 24
-    [TestCase("01844098762",1940)]
+    [TestCase("01814099829", 1940)]  // Intervallet 900-999 (Personer født mellom 1940 og 1999), Issue 24
+    [TestCase("01844098762", 1940)]
     [TestCase("03014595561", 1945)] // Intervallet 900-999 (Personer født mellom 1940 og 1999)
     [TestCase("18076591411", 1965)]
     [TestCase("28049064480", 1890)] // Intervallet 500-749 (Personer født mellom 1854 og 1899)
@@ -27,14 +26,13 @@ internal class ValidationTests
     [TestCase("07020554821", 2005)] // Intervallet 500-999 (Personer født mellom 2000 og 2039)
     [TestCase("12121064195", 2010)]
     [TestCase("22067510015", 1975)]  // Intervallet 000-499 (Personer født mellom 1900 og 1999)
-    [TestCase("15038512797",1985)]
+    [TestCase("15038512797", 1985)]
     public void Check_IsValidForIntervalsWithCorrectYear(string nin, int year)
     {
         Assert.That(nin.ErGyldigNin(false), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
         var ninchecker = new NinChecker(nin);
         Assert.That(ninchecker.RealYear, Is.EqualTo(year));
     }
-
 
     [TestCase("01814099829")]
     [TestCase("01844098762")]
@@ -52,20 +50,19 @@ internal class ValidationTests
         Assert.That(nin.ErGyldigNin(false), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
     }
 
-    [TestCase("200112345609",true)] // Duf
-    [TestCase("68126952442",true)] // Dnr
+    [TestCase("200112345609", true)] // Duf
+    [TestCase("68126952442", true)] // Dnr
     [TestCase("70090678378", true)]
     [TestCase("67016464373", true)]
     [TestCase("31739556891", false)] // Syntpop
-    [TestCase("28894698995",false)] // Tenor
+    [TestCase("28894698995", false)] // Tenor
     [TestCase("01112835470", true)] // Fnr
     [TestCase("01032078210", true)]
     [TestCase("01104343909", true)]
-    public void Check_IsValidInProduction(string nin,bool expected)
+    public void Check_IsValidInProduction(string nin, bool expected)
     {
-        Assert.That(nin.ErGyldigNin(true),Is.EqualTo(expected), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
+        Assert.That(nin.ErGyldigNin(true), Is.EqualTo(expected), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
     }
-
 
     [TestCase("130112345609")] // Duf
     [TestCase("68156952442")] // Dnr
@@ -80,13 +77,8 @@ internal class ValidationTests
     {
         var result = nin.ErGyldigNin(false);
         TestContext.WriteLine($"Reacted on {Validation.LastFailedStep}");
-        Assert.That(result,Is.False, $"{nin.FormatWith()} skulle vært ugyldig, failed at {Validation.LastFailedStep}");
+        Assert.That(result, Is.False, $"{nin.FormatWith()} skulle vært ugyldig, failed at {Validation.LastFailedStep}");
     }
-
-
-
-
-
 
     [TestCase("17054026641")]
     [TestCase("22095314442")]
@@ -105,7 +97,7 @@ internal class ValidationTests
                 $"{fnr.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
         });
     }
-    
+
     [TestCase("03923248608")]
     [TestCase("28894698995")]
     public void Can_validate_a_Tenornummer(string nin)
@@ -113,7 +105,6 @@ internal class ValidationTests
         Assert.That(nin.ErGyldigTenorTestNummer(), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
     }
 
-    
     [TestCase("31739556891")]
     public void Is_Invalid_Tenornummer(string nin)
     {
@@ -121,6 +112,7 @@ internal class ValidationTests
         TestContext.WriteLine($"Reacted on {Validation.LastFailedStep}");
         Assert.That(result, Is.False, $"{nin.FormatWith()} skal ikke være gyldig, failed at {Validation.LastFailedStep}");
     }
+
     [TestCase("44722264549")]
     [TestCase("31739556891")]
     public void Can_validate_a_SyntPopTestNumber(string nin)
@@ -135,8 +127,6 @@ internal class ValidationTests
         TestContext.WriteLine($"Reacted on {Validation.LastFailedStep}");
         Assert.That(result, Is.False, $"{nin.FormatWith()} skal ikke være gyldig, failed at {Validation.LastFailedStep}");
     }
-
-
 
     [TestCase("97054026641")]
     [TestCase("z7054026641")]
@@ -160,6 +150,7 @@ internal class ValidationTests
     {
         Assert.That(nin.ErGyldigDNummer(), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
     }
+
     [TestCase("91106297510")]
     [TestCase("21106297510")]
     [TestCase("45992528433")]
@@ -195,7 +186,7 @@ internal class ValidationTests
         var result = nin.ErGyldigHNummer();
         TestContext.WriteLine($"Reacted on {Validation.LastFailedStep}");
         // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-        if (nin!=null)
+        if (nin != null)
             Assert.That(result, Is.False, $"{nin.FormatWith()} skal ikke være gyldig, failed at {Validation.LastFailedStep}");
 
     }
@@ -208,7 +199,6 @@ internal class ValidationTests
 
     }
 
-
 #pragma warning disable NUnit1001 // The individual arguments provided by a TestCaseAttribute must match the type of the corresponding parameter of the method
     [TestCase(null)]
 #pragma warning restore NUnit1001 // The individual arguments provided by a TestCaseAttribute must match the type of the corresponding parameter of the method
@@ -220,17 +210,16 @@ internal class ValidationTests
         var result = nin.ErGyldigFHNummer();
         TestContext.WriteLine($"Reacted on {Validation.LastFailedStep}");
         // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-        if (nin!= null)
+        if (nin != null)
             Assert.That(result, Is.False, $"{nin.FormatWith()} skal ikke være gyldig, failed at {Validation.LastFailedStep}");
     }
-    
-    
-   // [TestCase("204167553610")]
+
+    // [TestCase("204167553610")]
     [TestCase("199201426900")]
     [TestCase("200112345609")]
     [TestCase("201017238203")]
     [TestCase("200816832910")]
-    
+
     public void Can_validate_a_dufnummer(string nin)
     {
         Assert.That(nin.ErGyldigDufNummer(), $"{nin.FormatWith()} var ugyldig, failed at {Validation.LastFailedStep}");
@@ -239,7 +228,7 @@ internal class ValidationTests
         Assert.That(ninchecker.RealYear, Is.EqualTo(year));
 
     }
-    
+
     [TestCase("71212121229")]
     [TestCase("")]
     [TestCase("1898z388020")]
@@ -262,16 +251,21 @@ internal class ValidationTests
         TestContext.WriteLine($" Dnr {c.IsDNummer}");
     }
 
-    [TestCase("17454026641","5/17/1940")] //HNr
-    [TestCase("51106297510","10/11/1962")]  // DNr
-    [TestCase("17054026641","5/17/1940")] // FNr
-    public void ThatBirthDayWorksForCorrectTypes(string nin,string expectedBirthDate)
+    [TestCaseSource(nameof(ThatBirthDayWorksForCorrectTypesInput))]
+    public void ThatBirthDayWorksForCorrectTypes(string nin, DateTime expectedBirthDate)
     {
         var c = new NinChecker(nin);
-        Assert.That(c.Birthdate.ToDateOnly().ToString(), Is.EqualTo(expectedBirthDate));
+        Assert.That(c.Birthdate, Is.EqualTo(expectedBirthDate));
         Assert.That(c.HasBirthdate, Is.True);
     }
-    
+
+    private static object[] ThatBirthDayWorksForCorrectTypesInput =
+    {
+        new object[] { "17454026641", new DateTime(1940, 5, 17) }, // HNr
+        new object[] { "51106297510", new DateTime(1962, 10, 11) }, // DNr
+        new object[] { "17054026641", new DateTime(1940, 5, 17) }, // FNr
+    };
+
     [TestCase("200112345609")] // Duf
     [TestCase("81212121223")]  // Fhn
     public void ThatBirthDayIgnoresInvalidTypes(string nin)
@@ -281,45 +275,11 @@ internal class ValidationTests
         Assert.That(c.HasBirthdate, Is.False);
     }
 
-    [TestCase("17454026641", "5/17/1940")] //HNr
-    [TestCase("51106297510", "10/11/1962")]  // DNr
-    [TestCase("17054026641", "5/17/1940")] // FNr
-    public void ThatBirthDayWorksForCorrectTypesUsingExtensionmethods(string nin, string expectedBirthDate)
-    {
-        Assert.That(nin.Birthdate().ToDateOnly().ToString(), Is.EqualTo(expectedBirthDate));
-        Assert.That(nin.HasBirthdate, Is.True);
-    }
-
     [TestCase("200112345609")] // Duf
     [TestCase("81212121223")]  // Fhn
     public void ThatBirthDayIgnoresInvalidTypesUsingExtensionmethods(string nin)
     {
         Assert.That(nin.Birthdate(), Is.Null);
         Assert.That(nin.HasBirthdate, Is.False);
-    }
-
-
-}
-
-public static class Extensions
-{
-    public static DateOnly? ToDateOnly(this DateTimeOffset? date)
-    {
-        return date != null ? DateOnly.FromDateTime(date.Value.Date) : null;
-    }
-
-    public static DateOnly ToDateOnly(this DateTimeOffset date)
-    {
-        return DateOnly.FromDateTime(date.Date);
-    }
-
-    public static DateOnly ToDateOnly(this DateTime date)
-    {
-        return DateOnly.FromDateTime(date.Date);
-    }
-
-    public static DateOnly? ToDateOnly(this DateTime? date)
-    {
-        return date!=null ?DateOnly.FromDateTime(date.Value.Date) : null;
     }
 }
